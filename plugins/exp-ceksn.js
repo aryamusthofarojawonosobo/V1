ff61ff06cf5676b3b9f8dbdf862a711abb7faf38
Azry <azry695@gmail.com>
2022-06-23 08:49:18 +0700

    Update exp-ceksn.js

--- plugins/exp-ceksn.js
+++ plugins/exp-ceksn.js
@@ -1,14 +1,15 @@
-import { createHash } from 'crypto'
+const { createHash } = require('crypto')
 
 let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
 let handler = async function (m, { conn, text, usedPrefix }) {
   let sn = createHash('md5').update(m.sender).digest('hex')
 
-m.reply(`*📮 SN:* ${sn}`)
+m.reply(`Note: Simpan *SN* dengan baik ya kak
+*📮 SN:* ${sn}`)
 }
 
 handler.help = ['ceksn']
 handler.tags = ['xp']
 handler.command = /^(ceksn)$/i
 handler.register = true
-export default handler
+module.exports = handler
