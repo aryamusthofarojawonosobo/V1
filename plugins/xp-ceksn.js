a6840d8af7f146bbe399519594b3b27e508e840a
Azry <azry695@gmail.com>
2022-06-23 08:51:53 +0700

    Update and rename exp-ceksn.js to xp-ceksn.js

--- plugins/xp-ceksn.js
+++ plugins/xp-ceksn.js
@@ -4,7 +4,7 @@ let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
 let handler = async function (m, { conn, text, usedPrefix }) {
   let sn = createHash('md5').update(m.sender).digest('hex')
 
-m.reply(`Note: Simpan *SN* dengan baik ya kak
+m.reply(`🤖 Note: Simpan *SN* dengan baik ya kak
 *📮 SN:* ${sn}`)
 }
 
