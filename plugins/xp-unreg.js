176f6519d46ffd15d8d155c3f3fdf064f06dc894
Azry <azry695@gmail.com>
2022-06-23 09:03:34 +0700

    Update and rename exp-unreg.js to xp-unreg.js

--- plugins/xp-unreg.js
+++ plugins/xp-unreg.js
@@ -1,11 +1,11 @@
-import { createHash } from 'crypto'
+const { createHash } = require('crypto')
 let handler = async function (m, { args }) {
   if (!args[0]) throw 'Serial Number kosong'
   let user = global.db.data.users[m.sender]
   let sn = createHash('md5').update(m.sender).digest('hex')
   if (args[0] !== sn) throw 'Serial Number salah'
   user.registered = false
-  m.reply('```Succes Unreg !```')
+  m.reply('Succes Unreg ! Sekarang kamu bisa daftar ulang')
 }
 handler.help = ['', 'ister'].map(v => 'unreg' + v + ' <SN|SERIAL NUMBER>')
 handler.tags = ['xp']
@@ -13,4 +13,4 @@ handler.tags = ['xp']
 handler.command = /^unreg(ister)?$/i
 handler.register = true
 
-export default handler
+module.exports = handler
